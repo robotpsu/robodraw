fbdde5fa67e196fdc52d8453230d10c0c768739c
Vasiliy Polyakov <vp@psu.ru>
2017-03-30 00:35:16 +0500

    release candidate 2
    - Implement command parsing. Commands are passed through a serial port.
    - Get rid of possible integer overflow in coordinates conversion.
    - Change line_to() to line() for using relative coordinates.
    - Add debugging output to a serial port (DEBUG).
    - Add speaker support (USE_BEEP).
    - Change constant names to upper case.
    - Remove unused functions.
    
    Signed-off-by: Vasiliy Polyakov <vp@psu.ru>

--- robodraw.ino
+++ robodraw.ino
@@ -6,107 +6,158 @@
 
 #include <Servo.h>
 
-/****  Type definitions  ****/
-/* A point in Cartesian coordinates */
-typedef struct {
-  int x, y;
-} Point;
-
 /****  Constants  ****/
-const int L1 = 110;            /* arms length in mm */
-const int L2 = 110;
-const int MoveDelay = 100;     /* movement delay in ms */
+/* Initial position in Cartesian coordinates (mm) */
+const int INIT_X =  0;
+const int INIT_Y = 20;
+/* Movement step (mm) */
+const int STEP = 8;
+/* Movement delay (ms) */
+const unsigned long DELAY_MOVE = 100;
+/* Arms length (mm) */
+const unsigned L1 = 110;
+const unsigned L2 = 100;
+/* Serial port data rate */
+const long SERIAL_SPEED = 9600;
+/* Shield pins */
+const unsigned PIN_SERVO1 = 10;
+const unsigned PIN_SERVO2 =  9;
+#ifdef USE_BEEP
+const unsigned PIN_BEEP = 7;
+#endif
 /* For use in coordinates conversion */
 const float Pi  = 3.14159265;  /* √-1 8 ∑ π */
-const float Deg = 180. / Pi;   /* radians to degrees conversion */
+const float DEG = 180. / Pi;   /* radians to degrees conversion */
 /* Some optimization */
-const int L11 = L1 * L1;
-const int L12 = L1 * L2 * 2;
-const int L22 = L2 * L2;
+const float L11 = (float)(L1 * L1);
+const float L12 = (float)(L1 * L2) * 2.;
+const float L22 = (float)(L2 * L2);
 
 /****  Global variables  ****/
 Servo Servo1, Servo2;  /* servos */
-float X, Y;            /* current position */
+int X, Y;              /* current position */
 
 /****  Initialization  ****/
 void setup() {
   /* Begin COMmunication */
-  Serial.begin(9600);
+  Serial.begin(SERIAL_SPEED);
 
   /* Attach servos to pins */
-  Servo1.attach(10);
-  Servo2.attach(9);
+  Servo1.attach(PIN_SERVO1);
+  Servo2.attach(PIN_SERVO2);
 
   /* Move to initial position */
-  move_to(0, 130);
+  move_to(INIT_X, INIT_Y);
 }
 
 /****  Main loop  ****/
 void loop() {
-  const int SquareSize = 20;
-  const int SquareNum  =  4;
+  char cmd;
 
-  for (int i = 0; i < SquareNum; i++) {
-    rect(SquareSize, SquareSize);
-    if (i < SquareNum - 1) line_v(SquareSize * 2);
+  if (Serial.available() > 0) {
+    cmd = Serial.read();
+    switch (cmd) {
+      case '0': move_to(INIT_X, INIT_Y); break;  /* reset */
+
+      case '2': line_v(-STEP); break;  /* North */
+      case '4': line_h( STEP); break;  /* East  */
+      case '6': line_v( STEP); break;  /* South */
+      case '8': line_h(-STEP); break;  /* West  */
+
+      case '1': line(-STEP, -STEP); break;  /* NW */
+      case '3': line( STEP, -STEP); break;  /* NE */
+      case '5': line( STEP,  STEP); break;  /* SE */
+      case '7': line(-STEP,  STEP); break;  /* SW */
+    }
   }
-
-  exit(0);
 }
 
 /****  Subroutines  ****/
+#ifdef USE_BEEP
+# define BEEP_OK beep_ok()
+# define BEEP_FAIL beep_fail()
+
+void beep_ok() {
+  tone(PIN_BEEP, 440, 200);
+}
+
+void beep_fail() {
+  tone(PIN_BEEP, 1200, 100);
+  delay(100);
+  tone(PIN_BEEP, 1200, 100);
+}
+#else
+# define BEEP_OK
+# define BEEP_FAIL
+#endif
 
 /*
- * Move the arm to the specified point.
+ * Move the arm directly to the specified point.
  *
  * Arguments: x, y - Cartesian coordinates (absolute)
  */
 void move_to(int x, int y) {
-  float r, r2, a1, a2;
+  /* The arm can operate in the right half-plane only */
+  if (y < 0) y = 0;
 
-  r2 = x * x + y * y;  /* squared distance */
-  r  = sqrt(r2);       /* distance */
+  float r = (float)x, r2 = (float)y;
+  r2 = r * r + r2 * r2;  /* squared distance */
+  r  = sqrt(r2);         /* distance */
 
-  if (r > L1 + L2 - 5) {
-    /* Safely fold the arm */
-    a1 = 180.; a2 = 20.;
-    x = 0; y = L1 + L2;
+  if (r > (float)(L1 + L2 - STEP)) {
+    /* The arm cannot reach the specified point */
+    BEEP_FAIL;
   } else {
     /* Convert Cartesian coordinates to servos angles */
-    a1 = 180. - (acos(x / r) - acos((L11 - L22 + r2) / (2 * L1 * r))) * Deg;
-    a2 = 180. - (Pi - acos((L11 + L22 - r2) / L12)) * Deg;
-  };
+    float c1 = (float)x / r;
+    float c2 = (L11 - L22 + r2) / (2. * (float)L1 * r);
+    float c3 = (L11 + L22 - r2) / L12;
+    float a1 = 180. - (acos(c1) - acos(c2)) * DEG;
+    float a2 = 180. - (Pi - acos(c3)) * DEG;
 
-  /* Set servos angles */
-  Servo1.write(a1);
-  Servo2.write(a2);
+#ifdef DEBUG
+    /* Debugging output to serial port */
+    Serial.print(x);
+    Serial.print("; ");
+    Serial.print(y);
+    Serial.print(" -> ");
+    Serial.print(a1);
+    Serial.print("; ");
+    Serial.print(a2);
+    Serial.println();
+#endif
 
-  /* Save current position */
-  X = x; Y = y;
+    /* Set servos angles */
+    Servo1.write(a1);
+    Servo2.write(a2);
+
+    /* Save current position */
+    X = x; Y = y;
+  }
 
   /* Stay awhile (and listen) */
-  delay(MoveDelay);
+  delay(DELAY_MOVE);
 }
 
 /*
  * Move the arm relative to current position.
  *
- * Arguments: dx, dy - coordinate deltas
+ * Arguments: dx, dy - end point coordinates (relative)
  */
-void move(float dx, float dy) {
+void move(int dx, int dy) {
   move_to(X + dx, Y + dy);
 }
 
 /*
  * Draw a horisontal line from current position.
  *
- * Arguments: x - line length and direction (sign)
+ * Arguments: x - end point X coordinate (relative)
  */
 void line_h(int dx) {
-  int d = dx > 0 ? 1 : -1;
-  int x0 = X + d, x1 = X + dx;
+  int s = dx > 0 ? 1 : -1;
+  int x0 = X + s, x1 = X + dx;
 
-  for (int i = x0; i != x1; i += d) {
+  for (int i = x0; i != x1; i += s) {
     move_to(i, Y);
   }
 }
@@ -114,13 +165,13 @@ void line_h(int dx) {
 /*
  * Draw a vertical line from current position.
  *
- * Arguments: y - line length and direction (sign)
+ * Arguments: y - end point Y coordinate (relative)
  */
 void line_v(int dy) {
-  int d = dy > 0 ? 1 : -1;
-  int y0 = Y + d, y1 = Y + dy;
+  int s = dy > 0 ? 1 : -1;
+  int y0 = Y + s, y1 = Y + dy;
 
-  for (int i = y0; i != y1; i += d) {
+  for (int i = y0; i != y1; i += s) {
     move_to(X, i);
   }
 }
@@ -129,41 +180,20 @@ void line_v(int dy) {
  * Draw a line from current position to the specified point
  * using Bresenheim algorithm.
  *
- * Arguments: x, y - Cartesian coordinates (absolute)
+ * Arguments: x, y - end point coordinates (relative)
  */
-void line_to(int x, int y) {
-  int dx = abs(x - X), sx = X < x ? 1 : -1;
-  int dy = abs(y - Y), sy = Y < y ? 1 : -1;
+void line(int x, int y) {
+  int x1 = X + x, dx = abs(x), sx = x > 0 ? 1 : -1;
+  int y1 = Y + y, dy = abs(y), sy = y > 0 ? 1 : -1;
   int e1 = (dx > dy ? dx : -dy) / 2, e2;
+  x = X; y = Y;
 
-  while (X != x || Y != y) {
+  for (;;) {
+    move_to(x, y);
+    if (x == x1 && y == y1) break;
     e2 = e1;
-    if (e2 > -dx) { e1 -= dy; X += sx; }
-    if (e2 <  dy) { e1 += dx; Y += sy; }
-    move_to(X, Y);
-  }
-}
-
-/*
- * Draw a rectangle with sides parallel to axes.
- *
- * Arguments: dx, dy - rectangle size (coordinate deltas)
- */
-void rect(int dx, int dy) {
-  line_h( dx);
-  line_v( dy);
-  line_h(-dx);
-  line_v(-dy);
-}
-
-/*
- * Draw a polygonal chain using array of points.
- *
- * Arguments: path[] - array of points
- */
-void polygon(Point path[]) {
-  for (int i = 0; i < sizeof(path); i++) {
-    line_to(path[i].x, path[i].y);
+    if (e2 > -dx) { e1 -= dy; x += sx; }
+    if (e2 <  dy) { e1 += dx; y += sy; }
   }
 }
 
